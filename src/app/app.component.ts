521f166de741cafff0ca5e19e2faff8289a7461a
TAMUTim <91436828+TAMUTim@users.noreply.github.com>
2023-01-28 13:20:33 -0600

    Correctly import animejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Tree-Time';
+
+  ngAfterViewInit(): void {
+    // Wrap every letter in a span
+    const textWrapper = document.querySelector('.an-1');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.an-1 .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70*i
+      }).add({
+        targets: '.an-1',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+  }
 }
+
+
